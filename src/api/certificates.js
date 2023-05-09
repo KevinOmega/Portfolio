6a5e0d8fbd289a58fb35fb4e5b489d345679d1ad
Kevin Huayllas <KevinOmega01@gmail.com>
2023-05-09 13:07:09 -0400

    add certificates api

---
+++ src/api/certificates.js
@@ -0,0 +1,37 @@
+export const certificates = [
+  {
+    id: 1,
+    name: "JavaScript ALgorithms and Data Structures",
+    brand: "FreeCodeCamp",
+    image: "/images/certificates/fcc.png",
+    url: "https://www.freecodecamp.org/certification/kevinomega01043/javascript-algorithms-and-data-structures",
+  },
+  {
+    id: 2,
+    name: "Responsive Web Design",
+    brand: "FreeCodeCamp",
+    image: "/images/certificates/fcc.png",
+    url: "https://www.freecodecamp.org/certification/kevinomega01043/responsive-web-design",
+  },
+  {
+    id: 3,
+    name: "Front ENd Development Libraries",
+    brand: "FreeCodeCamp",
+    image: "/images/certificates/fcc.png",
+    url: "https://www.freecodecamp.org/certification/kevinomega01043/front-end-development-libraries",
+  },
+  {
+    id: 4,
+    name: "The Complete Web Development Bootcamp",
+    brand: "Udemy",
+    image: "/images/certificates/udemy.png",
+    url: "https://www.udemy.com/certificate/UC-53ae470b-f604-4217-95c1-d44173fe764d/",
+  },
+  {
+    id: 5,
+    name: "Master the Coding INterview: Data Structure + Algorithms",
+    brand: "ZeroToMastery",
+    image: "/images/certificates/ztm.png",
+    url: "https://www.udemy.com/certificate/UC-5c60be16-b4de-43e7-89fe-067eb89f8bb0/",
+  },
+];
